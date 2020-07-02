4c8733eb6de55f40d693d2a88fd621b31172dd45
Tim Fleming <tfleming26@hotmail.com>
2020-07-02 15:29:38 -0600

    Re-install js dependencies

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -1,9 +1,14 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
 import React from "react";
 import { render } from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from "../components/App";
 
 document.addEventListener("DOMContentLoaded", () => {
-  render(<App />, document.body.appendChild(document.createElement("div")));
+  render(
+    <App />,
+    document.body.appendChild(document.createElement("div"))
+  );
 });
